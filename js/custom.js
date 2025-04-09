b3481557e80087c08d843cff0867fb41574e728b
Qadeer1813 <c00270632@itcarlow.ie>
2025-04-09 19:11:16 +0100

    Website layout complete need to finish adding the description and the features which then after I can upload this

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+});
+
 // to get current year
 function getYear() {
     var currentDate = new Date();
